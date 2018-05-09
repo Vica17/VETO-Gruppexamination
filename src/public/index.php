d9503a951250d4d71afe0ea798c9ce9152cd7a6b
Vica17 <vicanfend@gmail.com>
2018-05-09 13:08:00 +0200

    commes-routes-uppdate

--- src/public/index.php
+++ src/public/index.php
@@ -108,12 +108,23 @@ $app->group('/api', function () use ($app) {
       $allComments = $this->comment->getAll();
       return $response->withJson($allComments);
     });
-    $app->get('/comments/{amount}', function($request, $response, $args){
-      $allComments = $this->comment->getAll($args["amount"]);
-      return $response->withJson($allComments);
-    });
+
     $app->get('/comments/id/{commentID}', function($request, $response, $args){
       $allComments = $this->comment->getOne($args["commentID"]);
+      return $response->withJson($allComments);
+    });
+    $app->get('/entries/{id}/comments', function($request, $response, $args){
+      $allComments = $this->comment->getOne($args["commentID"]);
+      return $response->withJson($allComments);
+    });
+
+    $app->post('/comments', function ($request, $response, $args) {
+        $body = $request->getParsedBody();
+        $newComment = $this->comment->add($body);
+        return $response->withJson(['data' => $newComment]);
+    });
+    $app->delete('/comments/id/{commentID}', function($request, $response, $args){
+      $allComments = $this->comment->deleteOne($args["commentID"]);
       return $response->withJson($allComments);
     });
 
