7939b1af6c55363479fc6211d1e19844efddce62
Ellinor <enkran@hotmail.com>
2018-05-09 09:38:34 +0200

    more fixed to entrycontroller

--- src/App/Controllers/EntryController.php
+++ src/App/Controllers/EntryController.php
@@ -13,7 +13,7 @@ class EntryController{
 
   //get latest 20 entries
   public function getLatest(){
-    $getAll = $this->db->prepare('SELECT * FROM entries');
+    $getAll = $this->db->prepare('SELECT * FROM entries ORDER BY createdAt DESC LIMIT 20');
     $getAll->execute();
     return $getAll->fetchAll();
   }
@@ -25,11 +25,11 @@ class EntryController{
     return $getOne->fetch();
   }
 
-  //post
+  //Create new post
   public function add($title, $entry, $createdBy){
-
+    $date = date('Y-m-d H:i:s');
     $addOne = $this->db->prepare(
-        'INSERT INTO entries (content) VALUES (:content)'
+        'INSERT INTO entries (title, content, createdBy) VALUES (:title, :content, :createdBy, :createdAt)'
     );
     $addOne->execute([':content'  => $entry['content']]);
 
@@ -44,15 +44,28 @@ class EntryController{
     ];
   }
 
-  //delete
-  public function remove(){
-    $getOne = $this->db->prepare('DELETE * FROM entries WHERE id = :id');
-    $getOne->execute([':id' => $id]);
-    return $getOne->fetch();
+  //Delete
+  public function remove($entryID){
+    $getOne = $this->db->prepare(
+      "DELETE * FROM entries WHERE entryID = '$entryID'"
+    );
+    $statement->execute();
   }
 
-  //update
-  public function edit(){
+  //Edit post
+  public function edit($entryID){
+    $addOne = $this->db->prepare(
+        'INSERT INTO entries (title, content, createdBy) VALUES (:title, :content, :createdBy, :createdAt)'
+    );
+    $addOne->execute([':content'  => $entry['content']]);
 
+    return [
+      'id'          => (int)$this->db->lastInsertId(),
+      'title'       => $title,
+      'content'     => $entry['content'],
+      'createdBy'   => $createdBy,
+      'createdAt'   => $date,
+      'completed'   => false
+    ];
   }
 }
