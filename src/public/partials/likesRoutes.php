cb422a11da2db15f7d711a6769f766edafd26d08
Vica17 <vicanfend@gmail.com>
2018-05-16 11:08:40 +0200

    likesRoutes

--- src/public/partials/likesRoutes.php
+++ src/public/partials/likesRoutes.php
@@ -1,1 +1,17 @@
 <?php
+//post new like
+$app->post('/likes', function ($request, $response, $args) {
+$body = $request->getParsedBody();
+$newLike = $this->like->add($body);
+return $response->withJson(['data' => $newLike]);
+});
+//get all likes connected to entry
+$app->get('/entries/{id}/likes', function($request, $response, $args){
+$allLikes = $this->like->getOne($args["likeID"]);
+return $response->withJson($allLikes);
+});
+//remove like
+$app->delete('/likes/id/{likeID}', function($request, $response, $args){
+$allLikes = $this->like->deleteOne($args["likeID"]);
+return $response->withJson($allLikes);
+});
