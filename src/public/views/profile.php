3b512e8e6217109bcefdd4ce8edd39f6c7705a4a
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-05-19 14:13:20 +0200

    added profile page base

---
+++ src/public/views/profile.php
@@ -0,0 +1,10 @@
+<?= $username ?>
+
+<?php require 'components/head.php'; ?>
+
+<script src="/scripts/api.js"></script>
+<script src="/scripts/profile.js"></script>
+
+
+
+<?php require 'components/footer.php'; ?>
