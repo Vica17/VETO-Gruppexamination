a7bd5bfb0816158d5c959b037ca712e65dd3ba8d
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-05-21 12:48:17 +0200

    profile and buildData start

--- src/public/scripts/profile.js
+++ src/public/scripts/profile.js
@@ -1,4 +1,4 @@
-async function test(){
+async function createProfile(){
   // get username
   let username = window.location.pathname.split("/").slice(-1)[0];
 
@@ -6,16 +6,84 @@ async function test(){
   let user = await api.fetchData("users/username/" + username);
   console.log(user);
 
-  // if user exists
-  if(user != false){
-    console.log("User found");
-    console.log(user["username"]);
-  } else {
-    console.log("User not found");
+  // if user does not exists
+  if(user == false){ return false; }
+
+  let entries = await api.fetchData("entries/user/" + user["userID"]);
+  let comments = await api.fetchData("users/" + user["userID"] + "/comments");
+  let likes = await api.fetchData("users/" + user["userID"] + "/likes");
+
+  console.log(entries);
+  console.log(comments);
+  console.log(likes);
+
+  // if X is not empty -> print
+  if(entries[0] != null){
+    // create menu button
+    let menu = document.getElementById("profile-menu");
+    let button = document.createElement("button");
+    button.innerHTML = "All Entries";
+    menu.appendChild(button);
+
+    // create all entries
+    let entries = document.getElementById("profile-entries");
+
   }
+  if(comments[0] != null){
+    let menu = document.getElementById("profile-comments");
+    let button = document.createElement("button");
+    button.innerHTML = "All Comments";
+    menu.appendChild(button);
+
+  }
+  if(likes[0] != null){
+    let container = document.getElementById("profile-likes");
+    let menu = document.getElementById("profile-menu");
+
+    let button = document.createElement("button");
+    button.innerHTML = "All Likes";
+    menu.appendChild(button);
+
+    // create all likes
+    likes.forEach(function (like) {
+      let d = buildData.like(like);
+      container.appendChild(d);
+    });
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Patch Function
+
+    // let entryID = 11;
+    // let data = {
+    //   "title": "This is the title",
+    //   "content": "This data was sent with javascript again again again !!!!",
+    //   "createdBy": 1
+    // };
+
+    // api.patchData("entries/" + entryID, data);
+
 }
 
-test();
+createProfile();
 
   // get user info
   // get elements
