833b7732b3fa08ad9962b3175c3b4e13e77ada28
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-05-21 17:24:35 +0200

    more like button

--- src/public/scripts/functions.js
+++ src/public/scripts/functions.js
@@ -1,6 +1,20 @@
 async function likePost(e){
+
+  let entryID = e.target.elements["entryID"].value;
+  let userID = e.target.elements["userID"].value;
+
   let data = {
-    "entryID": e.target.elements["entryID"].value
+    "entryID": entryID
   };
-  let d = await api.postData("likes", data);
+  api.postData("likes", data);
+
+
+
+  // if user has already liked -> remove like
+  // else -> add like
+
+  let question = await api.fetchData("entries/" + entryID + "/likes");
+  console.log(question);
+
+
 }
