ecee3150c8d8aa9bbd80dd097d0b57a83aad4444
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-05-22 14:59:08 +0200

    serach page + nav start

--- src/public/views/components/nav.php
+++ src/public/views/components/nav.php
@@ -1,6 +1,12 @@
-<!-- <div class="nav">
+<div class="nav">
   <div class="nav-left">
     <li> <a href="/">Home</a> </li>
     <li> <a href="/Documentation">Documentation</a> </li>
   </div>
-</div> -->
+  <div class="nav-right">
+    <form class="" action="/search" method="GET">
+      <input type="search" name="key">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</div>
